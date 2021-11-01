a8958867d422ee2e5eeb6d8b40f4a5f46eeef1c6
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2021-11-01 08:43:32 -0400

    Create tTest.java

---
+++ Stats_Func/tTest.java
@@ -0,0 +1,43 @@
+package Stats_Func;
+
+public class tTest {
+    public static void main(String[] args) {
+        
+    }
+
+    public static double correlated(double[][] ar,int rowMin1, int rowMax1, int col1, int rowMin2, int rowMax2, int col2){
+        double result=0;
+
+        return result;
+    }
+
+    public static double correlatedAll(double[][] ar, int col1, int col2){
+        double result=0;
+        result=correlated(ar, 0, ar.length, col1, 0, ar.length, col2);
+        return result;
+    }
+
+    public static double variance(double[][] ar,int rowMin1, int rowMax1, int col1, int rowMin2, int rowMax2, int col2){
+        double result=0;
+        
+        return result;
+    }
+
+    public static double vairanceAll(double[][] ar, int col1, int col2){
+        double result=0;
+        result=variance(ar, 0, ar.length, col1, 0, ar.length, col2);
+        return result;
+    }
+
+    public static double unequal(double[][] ar,int rowMin1, int rowMax1, int col1, int rowMin2, int rowMax2, int col2){
+        double result=0;
+        
+        return result;
+    }
+
+    public static double unequalAll(double[][] ar, int col1, int col2){
+        double result=0;
+        result=unequal(ar, 0, ar.length, col1, 0, ar.length, col2);
+        return result;
+    }
+}
