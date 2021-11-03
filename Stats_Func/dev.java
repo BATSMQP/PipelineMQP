3781dfec2641557b29ef852e34abd5d1718943ef
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2021-11-03 14:07:46 -0400

    Creation of dev

---
+++ Stats_Func/dev.java
@@ -0,0 +1,16 @@
+package Stats_Func;
+
+public class dev {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public static double Array(double[][] ar, int rowMin, int rowMax, int col){
+
+        double result=0;
+
+        return result;
+    }
+
+}
--- Stats_Func/mean.java
+++ Stats_Func/mean.java
@@ -2,6 +2,7 @@ package Stats_Func;
 public class mean{
 
     public static void main(String[] args) {
+        //This is basicaly a test, but I couldn't find the easy test method.
         double m;
         double[][] ar=new double [4][5];
         ar[0][0]=2;
@@ -17,7 +18,7 @@ public class mean{
     }
 
     public static double Array(double[][] ar, int rowMin, int rowMax, int col){
-
+        //this takes the array, the min max rows, and the col and gets a mean of that area
         double total = 0;
 
         for(int i=rowMin; i<=rowMax; i++){
@@ -29,7 +30,7 @@ public class mean{
 
 
     public static double All(double[][] ar,int col){
-
+        //this simplifyes the other method, just taking the full length of the col
         double total;
         total=Array(ar,0,ar.length-1,col);
         return total;
