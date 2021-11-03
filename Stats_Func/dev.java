3e687efeffe4a557204202ee3b4cbe9923aa9e1d
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2021-11-03 14:50:27 -0400

    varable standarization and start of ttest propper

--- Stats_Func/dev.java
+++ Stats_Func/dev.java
@@ -6,7 +6,7 @@ public class dev {
 
     public static void main(String[] args) {
         //This is basicaly a test, but I couldn't find the easy test method.
-        
+
         double m;
         double[][] ar=new double [4][5];
         ar[0][0]=2;
@@ -14,20 +14,20 @@ public class dev {
         ar[2][0]=4;
         ar[3][0]=5;
 
-        m= Var(ar,0,2,0);
+        m= var(ar,0,2,0);
         System.out.println(m);
 
-        m= AllVar(ar,0);
+        m= varAll(ar,0);
         System.out.println(m);
 
         m= SD(ar,0,2,0);
         System.out.println(m);
 
-        m= AllSD(ar,0);
+        m= SDAll(ar,0);
         System.out.println(m);
     }
 
-    public static double Var(double[][] ar, int rowMin, int rowMax, int col){
+    public static double var(double[][] ar, int rowMin, int rowMax, int col){
         //takes variance of a group of cells
 
         double result=0;
@@ -40,10 +40,10 @@ public class dev {
         return result/(rowMax-rowMin+1);
     }
 
-    public static double AllVar(double[][] ar,int col){
+    public static double varAll(double[][] ar,int col){
         //this simplifyes the other method, just taking the full length of the col
         double total;
-        total=Var(ar,0,ar.length-1,col);
+        total=var(ar,0,ar.length-1,col);
         return total;
 
     }
@@ -51,11 +51,11 @@ public class dev {
     public static double SD(double[][] ar, int rowMin, int rowMax, int col){
         //takes the square root of the variance
         double result=0;
-        result=Math.sqrt(Var(ar,rowMin,rowMax,col));
+        result=Math.sqrt(var(ar,rowMin,rowMax,col));
         return result;
     }
 
-    public static double AllSD(double[][] ar,int col){
+    public static double SDAll(double[][] ar,int col){
         //this simplifyes the other method, just taking the full length of the col
         double total;
         total=SD(ar,0,ar.length-1,col);
--- Stats_Func/tTest.java
+++ Stats_Func/tTest.java
@@ -8,15 +8,24 @@ public class tTest {
         
     }
 
-    public static double correlated(double[][] ar,int rowMin1, int rowMax1, int col1, int rowMin2, int rowMax2, int col2){
+    public static double paired(double[][] ar,int rowMin1, int rowMax1, int col1, int rowMin2, int rowMax2, int col2){
         double result=0;
+        double m1=mean.Array(ar,rowMin1,rowMax1,col1);
+        double m2=mean.Array(ar,rowMin2,rowMax2,col2);
+        int n=rowMax1-rowMin1+1;
+
+        double[] difAr= new double[n];
+
+        for(int i=0; i<n;i++){
+            difAr[i]=Math.abs(0); //placeholder should be matched value 1-2
+        }
 
         return result;
     }
 
-    public static double correlatedAll(double[][] ar, int col1, int col2){
+    public static double pairedAll(double[][] ar, int col1, int col2){
         double result=0;
-        result=correlated(ar, 0, ar.length, col1, 0, ar.length, col2);
+        result=paired(ar, 0, ar.length, col1, 0, ar.length, col2);
         return result;
     }
 
