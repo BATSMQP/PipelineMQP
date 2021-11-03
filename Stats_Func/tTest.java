2d0c89c50ec74fb9ae33de97aa4391a649054c9e
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2021-11-03 15:39:13 -0400

    Update tTest.java

--- Stats_Func/tTest.java
+++ Stats_Func/tTest.java
@@ -5,7 +5,24 @@ public class tTest {
     //TODO: make array able to have nondoubles but ignore them
 
     public static void main(String[] args) {
-        
+        //This is basicaly a test, but I couldn't find the easy test method.
+        double m;
+        double[][] ar=new double [4][5];
+        ar[0][0]=2;
+        ar[1][0]=3;
+        ar[2][0]=4;
+        ar[3][0]=5;
+
+        ar[0][1]=2;
+        ar[1][1]=3;
+        ar[2][1]=4;
+        ar[3][1]=4;
+
+        m= paired(ar,0,2,0,0,2,1);
+        System.out.println(m);
+
+        m= pairedAll(ar,0,1);
+        System.out.println(m);
     }
 
     public static double paired(double[][] ar,int rowMin1, int rowMax1, int col1, int rowMin2, int rowMax2, int col2){
@@ -14,11 +31,15 @@ public class tTest {
         double m2=mean.Array(ar,rowMin2,rowMax2,col2);
         int n=rowMax1-rowMin1+1;
 
-        double[] difAr= new double[n];
+        double[][] difAr= new double[n][1];
 
         for(int i=0; i<n;i++){
-            difAr[i]=Math.abs(0); //placeholder should be matched value 1-2
+            difAr[i][0]=Math.abs(ar[rowMin1][col1]-ar[rowMin2][col2]); //placeholder should be matched value 1-2
         }
+
+        double SD=dev.SDAll(difAr, 0);
+
+        result=(m1-m2)/(SD/Math.sqrt(n));
 
         return result;
     }
