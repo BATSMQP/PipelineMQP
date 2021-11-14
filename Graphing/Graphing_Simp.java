f11760018511e05eadeab03462b181029ac5f3f8
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2021-11-14 15:08:41 -0500

    Graphing 2.0

--- Graphing/Graphing_Simp.java
+++ Graphing/Graphing_Simp.java
@@ -4,8 +4,10 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.geom.*;
 
+//https://www.codespeedy.com/plot-graph-in-java/
+
 public class Graphing_Simp extends JPanel{
-    int[] coordinates={100,20};
+    public static double [][] coordinates=new double[100][2];
     int mar=50;
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -18,20 +20,21 @@ public class Graphing_Simp extends JPanel{
         double x=(double)(width-2*mar)/(coordinates.length-1);
         double scale=(double)(height-2*mar)/getMax();
         g1.setPaint(Color.BLUE);
+
         for(int i=0;i<coordinates.length;i++){
-            double x1=mar+i*x;
-            double y1=height-mar-scale*coordinates[i];
+            double x1=mar+coordinates[i][0]*x;
+            double y1=height-mar-scale*coordinates[i][1];
             g1.fill(new Ellipse2D.Double(x1-2,y1-2,4,4));
         }
         
         
         
     }
-    private int getMax(){
-        int max=-Integer.MAX_VALUE;
+    private double getMax(){
+        double max=-Integer.MAX_VALUE;
         for(int i=0;i<coordinates.length;i++){
-            if(coordinates[i]>max)
-                max=coordinates[i];
+            if(coordinates[i][1]>max)
+                max=coordinates[i][1];
            
         }return max;
     }       
@@ -39,9 +42,16 @@ public class Graphing_Simp extends JPanel{
     public static void main(String args[]){
         JFrame frame =new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Graphing_Simp.coordinates[0][0]= 3;
+    Graphing_Simp.coordinates[0][1]= 4;
+    Graphing_Simp.coordinates[1][0]= 6;
+    Graphing_Simp.coordinates[1][1]= 4;
+    Graphing_Simp.coordinates[2][0]= 6;
+    Graphing_Simp.coordinates[2][1]= 2;
     frame.add(new Graphing_Simp());
     frame.setSize(400,400);
     frame.setLocation(200,200);
     frame.setVisible(true);
     }
+
 }
