526fbf3baec10a2f072b45ab5b71c2125247b79a
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2021-11-21 12:16:07 -0500

    Demo2.0 done

--- Stats_Func/Demo.java
+++ Stats_Func/Demo.java
@@ -1,12 +1,20 @@
 package Stats_Func;
 
+import java.util.concurrent.TimeUnit;
+
 import Gen_Algo.ReadFile;
+import Graphing.Graphing_Simp;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         double m[];
         double b;
-        double[][] ar=ReadFile.fromCSVtoD2("Data\\Test.csv", 0, 1);
+        double[][] ar=ReadFile.fromCSVtoD2("Data\\Test.csv", 1, 2);
+        double[][] t1=ReadFile.fromCSVtoD2("Data\\Test.csv", 0, 1);
+        double[][] t2=ReadFile.fromCSVtoD2("Data\\Test.csv", 0, 2);
+        Graphing_Simp.printThisD2(t1);
+        TimeUnit.SECONDS.sleep(10);
+        Graphing_Simp.printThisD2(t2);
 
         /* m= tTest.paired(ar,0,2,0,0,2,1);
         System.out.println(m[0]);
