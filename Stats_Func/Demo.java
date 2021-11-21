4216409782c06e47b6a48154adeaa358bfd25259
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2021-11-21 12:01:46 -0500

    Demo redone

--- Stats_Func/Demo.java
+++ Stats_Func/Demo.java
@@ -8,7 +8,32 @@ public class Demo {
         double b;
         double[][] ar=ReadFile.fromCSVtoD2("Data\\Test.csv", 0, 1);
 
+        /* m= tTest.paired(ar,0,2,0,0,2,1);
+        System.out.println(m[0]);
+        System.out.println(m[1]);
 
+        m= tTest.pairedAll(ar,0,1);
+        System.out.println(m[0]);
+        System.out.println(m[1]); 
+
+        b = m[2];
+        System.out.printf("The p value is %f\n",b);  */   
+
+
+        /* m= tTest.sameSize(ar,0,2,0,0,2,1);
+        System.out.println(m[0]);
+        System.out.println(m[1]);
+
+        b = m[2];
+        System.out.printf("The p value is %f\n",b);
+
+        m= tTest.sameSizeAll(ar,0,1);
+        System.out.println(m[0]);
+        System.out.println(m[1]); 
+
+        b = m[2];
+        System.out.printf("The p value is %f\n",b); */  
+        
         m= tTest.unequal(ar,0,2,0,0,2,1);
         System.out.println(m[0]);
         System.out.println(m[1]);
