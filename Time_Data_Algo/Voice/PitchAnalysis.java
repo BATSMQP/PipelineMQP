ab8fe47cc59402bf11556a3cea1f63f586bcfdfd
canicolas <66385219+canicolas@users.noreply.github.com>
2021-11-21 19:52:29 -0500

    Starting to create the pitch analysis

---
+++ Time_Data_Algo/Voice/PitchAnalysis.java
@@ -0,0 +1,7 @@
+package Time_Data_Algo.Voice;
+
+public class PitchAnalysis {
+    
+    //function that runs a pitch test
+    //function that uses pitch test w/ different sliding windows=> would help understand how pitch changes over time
+}
