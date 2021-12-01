5e182d6d4100e0d9f9a5f4f1a292791d51727d4e
canicolas <66385219+canicolas@users.noreply.github.com>
2021-12-01 15:59:59 -0500

    Go lauren! go Lauren!

--- Interpreter/Interpreter.java
+++ Interpreter/Interpreter.java
@@ -1,11 +1,25 @@
 package Interpreter;
 import java.io.*;
 
+import org.python.util.PythonInterpreter; 
+import org.python.core.*; 
+
 public class Interpreter{
     public static void main(String[] args){
-        try{
+        PythonInterpreter python = new PythonInterpreter();
+        int number1 = 10;
+        int number2 = 32;
+        
+        python.set("number1", new PyInteger(number1));
+        python.set("number2", new PyInteger(number2));
+        python.exec("number3 = number1+number2");
+        PyObject number3 = python.get("number3");
+        System.out.println("val : "+number3.toString());
+        }
+}
+       /*  try{
             String prg= "import sys\nprint int(sys.argv[1]+int(sys.argv[2])\n";
-            BufferedWriter out- new BufferedWriter(new FileWriter("HelloWorld.py"));
+            BufferedWriter out= new BufferedWriter(new FileWriter("HelloWorld.py"));
             out.write(prg);
             out.close();
             int number1= 10;
@@ -15,11 +29,12 @@ public class Interpreter{
             Process p= pb.start();
 
             BufferedReader in= new BufferedReader(new InputStreamReader(p.getInputStream()));
-            int ret = new Integer(in.readLine()).intValue();
+            int ret = (in.readLine()).intValue();
             System.out.println("value is :"+ret);
             
-        }Catch(Exception e) {System.out.println(e);}
-    }
+        }
+        catch(Exception e) {System.out.println(e);} */
+    //}
 
 
-}
+//}
