7d9fad24c335ce5554fa5ce63e79295777fce59d
lewasserman <laurenewasserman@gmail.com>
2021-12-13 18:16:09 -0500

    filepath->file

--- BATS/src/main/java/com/amazonaws/lambda/demo/http/NewDataRequest.java
+++ BATS/src/main/java/com/amazonaws/lambda/demo/http/NewDataRequest.java
@@ -6,26 +6,26 @@ import com.amazonaws.lambda.demo.model.Study;
 
 /** To work with AWS must not have final attributes, must have no-arg constructor, and all get/set methods. */
 public class NewDataRequest {
-	String filepath;
+	String file;
 	String name;
 	String dataType;
 	ArrayList<Study> studies = new ArrayList<Study>();
 
-	public String getFilepath() { return filepath; }
+	public String getFile() { return file; }
 	public String getName() { return name;}
 	public String getDataType() { return dataType;}
 	public ArrayList<Study> getStudies() { return studies;}
-	public void setFilepath(String filepath) { this.filepath = filepath; }
+	public void setFile(String file) { this.file = file; }
 	public void setName(String name) { this.name = name; }
 	public void setDataType(String dataType) { this.dataType = dataType; }
 	public void setStudies(ArrayList<Study> studies) {this.studies = studies;}
 	
 	public String toString() {
-		return "NewDataRequest(" + filepath + ", "+ name + ", " + dataType + ", " + studies + ")";
+		return "NewDataRequest(" + name + ", " + dataType + ", " + studies + ")";
 	}
 	
-	public NewDataRequest (String filepath, String name, String dataType, ArrayList<Study> studies) {
-		this.filepath = filepath;
+	public NewDataRequest (String file, String name, String dataType, ArrayList<Study> studies) {
+		this.file = file;
 		this.name = name;
 		this.dataType = dataType;
 		this.studies = studies;
