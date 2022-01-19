9600c1859f199ee541eca0af89035e9e6a57ec83
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2022-01-19 13:01:35 -0500

    csv writer, test 1 done

---
+++ Gen_Algo/Table2CSV.java
@@ -0,0 +1,44 @@
+package Gen_Algo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Table2CSV {
+    public static void main(String[] args) {
+        String[][] a=new String[4][2];
+        a[0][0]="0.0";
+        a[1][1]="1.1";
+        col2("test", a);
+        
+    }
+
+    public static void col2(String name, String[][] a) {
+        try{
+            File file = new File("./Data/",name+".csv" ); //KEY IS DIR ex."./local-storage/" and fileName='comp.html'
+
+            // if file doesnt exists, then create it 
+            if ( ! file.exists( ) )
+            {
+                file.createNewFile( );
+            }
+    
+            FileWriter fw = new FileWriter( file.getAbsoluteFile( ) );
+            BufferedWriter fileWriter = new BufferedWriter(fw);
+            int l=0;
+            while(a.length>l){
+                String line = String.format("%s,%s", a[l][0],a[l][1]);
+                    
+                fileWriter.newLine();
+                fileWriter.write(line); 
+                l++;
+            }
+            fileWriter.close();
+        } catch (IOException e){
+            System.out.println("File IO error:");
+            e.printStackTrace();
+        }
+        return;
+    }
+}
