4a19e822ce862c3c7742e72d00e3cc044540156c
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2022-01-19 15:12:01 -0500

    more testing and csv creation improvements

--- Gen_Algo/Table2CSV.java
+++ Gen_Algo/Table2CSV.java
@@ -7,19 +7,20 @@ import java.io.IOException;
 
 public class Table2CSV {
     public static void main(String[] args) {
-        String[][] a=new String[4][2];
+        String[][] a=new String[4][3];
         a[0][0]="0.3";
         a[1][1]="1.3";
-        Stri("test", a,2);
+        a[2][2]="2.3";
+        Stri("test", a,3);
 
-        double[][] b=new double[4][2];
+        double[][] b=new double[4][3];
         b[0][0]=0.3;
         b[1][1]=1.3;
-        Doub("test_d", b,2);
+        b[2][2]=2.3;
+        Doub("test_d", b,3);
     }
 
     public static void Stri(String name, String[][] a, int colNum) {
-        //TODO: make work for all col numbers, just a string apend thing
         //TODO: naming conventions improve
         try{
             File file = new File("./Data/",name+".csv" ); //KEY IS DIR ex."./local-storage/" and fileName='comp.html'
@@ -32,13 +33,15 @@ public class Table2CSV {
     
             FileWriter fw = new FileWriter( file.getAbsoluteFile( ) );
             BufferedWriter fileWriter = new BufferedWriter(fw);
-            int l=0;
-            while(a.length>l){
-                String line = String.format("%s,%s", a[l][0],a[l][1]);
-                    
+            
+            for(int i=0; a.length>i;i++){
+                String line =a[i][0];
+                for(int j=1;j<colNum;j++){
+                    line = line+","+a[i][j];
+                }
                 fileWriter.newLine();
                 fileWriter.write(line); 
-                l++;
+                
             }
             fileWriter.close();
         } catch (IOException e){
--- UpperLevel/RunAlgo.java
+++ UpperLevel/RunAlgo.java
@@ -10,8 +10,9 @@ public class RunAlgo {
         if (algo=="lowpass"){
             Scanner keyboard = new Scanner(System.in); 
             Gen_Algo.TimeSeriesData Data= new Gen_Algo.TimeSeriesData(Gen_Algo.ReadFile.fromCSVtoD2(f.getAbsolutePath(),0, 1));
-            System.out.print("What is the cutoff signal you wish to use for the filter (<.50): ");  
-            double cutOff= keyboard.nextDouble();
+            //System.out.print("What is the cutoff signal you wish to use for the filter (<.50): ");  
+            //double cutOff= keyboard.nextDouble();
+            double cutOff= .3;
             double[][] LowPassed= Butterworth.LowPass(Data,0, Data.GetTime().length, 1, 1,  cutOff); //fs and order are bs just for this version
             //Graphing.Graphing_Simp.printThisD2(LowPassed,j2);
             Gen_Algo.Table2CSV.Doub(f.getName()+"_lowpassed", LowPassed,2);
