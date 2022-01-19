4080208e037cf969332d352f1f2a03c3f371e4b3
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2022-01-19 15:45:33 -0500

    Update RunAlgo.java

--- UpperLevel/RunAlgo.java
+++ UpperLevel/RunAlgo.java
@@ -7,8 +7,9 @@ import Gen_Algo.Butterworth;
 
 public class RunAlgo {
     public static void run(File f,String algo){
+        //TODO: Make accedental overwriting less of an issue
         if (algo=="lowpass"){
-            Scanner keyboard = new Scanner(System.in); 
+            //Scanner keyboard = new Scanner(System.in); 
             Gen_Algo.TimeSeriesData Data= new Gen_Algo.TimeSeriesData(Gen_Algo.ReadFile.fromCSVtoD2(f.getAbsolutePath(),0, 1));
             //System.out.print("What is the cutoff signal you wish to use for the filter (<.50): ");  
             //double cutOff= keyboard.nextDouble();
@@ -16,10 +17,12 @@ public class RunAlgo {
             double[][] LowPassed= Butterworth.LowPass(Data,0, Data.GetTime().length, 1, 1,  cutOff); //fs and order are bs just for this version
             //Graphing.Graphing_Simp.printThisD2(LowPassed,j2);
             Gen_Algo.Table2CSV.Doub(f.getName()+"_lowpassed", LowPassed,2);
+            
         }else{
             System.out.println("This algorithm is not curently available");
+            return;
         }
-
+        System.out.println("File "+f.getName()+" had the "+algo+ " algorithm run on it.\nThe result is in the Data folder.");
     }
 
     public static void main(String[] args) {
