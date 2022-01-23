8af5558d0de2d4e73874f587fd129fc6cd9bd66f
lewasserman <laurenewasserman@gmail.com>
2022-01-23 14:04:44 -0500

    api

--- BATS/js/api.js
+++ BATS/js/api.js
@@ -4,6 +4,7 @@ var base_url = 'https://es7rigcmle.execute-api.us-east-2.amazonaws.com/beta/';
 
 var CreateStudy_url = base_url + 'CreateStudy'; //POST
 var GetData_url = base_url + 'GetData'; //POST
+var GetStudyData_url = base_url + 'GetStudyData'; //POST
 var getStudies_url = base_url + 'getStudies'; //POST
 var Login_url = base_url + 'Login'; //POST
 var NewData_url = base_url + 'NewData'; //POST
@@ -11,3 +12,4 @@ var NewTool_url = base_url + 'NewTool'; //POST
 var Register_url = base_url + 'Register'; //POST
 var saveAnalysis_url = base_url + 'saveAnalysis'; //POST
 var GetUsername_url = base_url + 'GetUsername'; //POST
+var RunAlg_url = base_url + 'RunAlg'; //POST
