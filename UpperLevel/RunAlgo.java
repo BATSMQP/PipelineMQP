98932e8ca942b444f378c9f3cb4ff36a4c73ed09
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2022-02-01 16:55:39 -0500

    jar v3

--- UpperLevel/RunAlgo.java
+++ UpperLevel/RunAlgo.java
@@ -7,8 +7,9 @@ import Gen_Algo.Butterworth;
 //import Interpreter.PyInterpreter;
 
 public class RunAlgo {
-    public static void run(File f,String algo){
+    public static File run(File f,String algo){
         //TODO: Make accedental overwriting less of an issue
+        File file;
         if (algo=="lowpass"){
             //Scanner keyboard = new Scanner(System.in); 
             Gen_Algo.TimeSeriesData Data= new Gen_Algo.TimeSeriesData(Gen_Algo.ReadFile.fromCSVtoD2(f.getAbsolutePath(),0, 1));
@@ -17,7 +18,7 @@ public class RunAlgo {
             double cutOff= .3;
             double[][] LowPassed= Butterworth.LowPass(Data,0, Data.GetTime().length, 1, 1,  cutOff); //fs and order are bs just for this version
             //Graphing.Graphing_Simp.printThisD2(LowPassed,j2);
-            Gen_Algo.Table2CSV.Doub(f.getName()+"_lowpassed", LowPassed,2,f);
+            file=Gen_Algo.Table2CSV.Doub(f.getName()+"_lowpassed", LowPassed,2,f);
             
         }else if(algo=="ttest"){
             double[][] ar=Gen_Algo.ReadFile.fromCSVtoD2(f.getAbsolutePath(),0, 1);
@@ -27,21 +28,23 @@ public class RunAlgo {
             m2[0][0]=m[0];
             m2[1][0]=m[1];
             m2[2][0]=m[2];
-            Gen_Algo.Table2CSV.Doub(f.getName()+"_ttested",m2,1,f);
+            file=Gen_Algo.Table2CSV.Doub(f.getName()+"_ttested",m2,1,f);
 
         //}else if(algo=="graphwav"){
         //    PyInterpreter.graphWav();
         //    return;
         }else{
             System.out.println("This algorithm is not curently available");
-            return;
+            return f;
         }
         System.out.println("File "+f.getName()+" had the "+algo+ " algorithm run on it.\nThe result is in the Data folder.");
+        return file;
     }
 
     public static void main(String[] args) {
         String name="test_d";
         File file = new File("./Data2/",name+".csv" );
-        run(file, "lowpass");
+        File fileout=run(file, "lowpass");
+        System.out.println(fileout.getName());
     }
 }
